dcef39f27cd8828b0f74cab441feb5c16f9c0e97
kite999 <skyfish66@gmail.com>
2018-05-22 14:54:07 +0900

    initial commit

---
+++ src/main/java/com/example/s3Access/S3AccessApplication.java
@@ -0,0 +1,12 @@
+package com.example.s3Access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3AccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3AccessApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/s3Access/S3AccessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.s3Access;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class S3AccessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
